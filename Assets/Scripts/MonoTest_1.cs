8c5b5f01a057f984408e97087b2f52a78b9fe3fb
Wes <weskao@yile.com.tw>
2022-03-10 17:32:59 +0800

    Add Wheel example.

---
+++ Assets/Scripts/MonoTest_1.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Project.Movement
+{
+    public class MonoTest_1 : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.LogFormat("<color=yellow>Wes - [MonoTest_1] MonoTest_1</color>");
+        }
+    }
+}
---
+++ Assets/Scripts/MonoTest_2.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Project.Movement
+{
+    public class MonoTest_2 : MonoBehaviour
+    {
+        private void Awake()
+        {
+            Debug.LogFormat("<color=green>Wes - [MonoTest_2] MonoTest_2</color>");
+        }
+    }
+}
