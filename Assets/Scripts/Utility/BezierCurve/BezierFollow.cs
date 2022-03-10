eb09f6c086c05407854bcd3c71c2d2649614af41
Wes <weskao@yile.com.tw>
2022-03-10 17:32:19 +0800

    Add Besizer curve example.

---
+++ Assets/Scripts/Utility/BezierCurve/BezierFollow.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace PocketLobby.Utility
+{
+    // reference: https://www.youtube.com/watch?v=11ofnLOE8pw
+    public class BezierFollow : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] routes = null;
+
+        private int _routeToGo;
+        private float _tParam;
+        private Vector2 _targetObjectPosition;
+        private float _speedModifier;
+        private bool _coroutineAllowed;
+
+        private void Start()
+        {
+            _routeToGo = 0;
+            _tParam = 0f;
+            _speedModifier = 0.5f;
+            _coroutineAllowed = true;
+        }
+
+        private void Update()
+        {
+            if (_coroutineAllowed)
+            {
+                StartCoroutine(GoByTheRoute(_routeToGo));
+            }
+        }
+
+        private IEnumerator GoByTheRoute(int routeNumber)
+        {
+            _coroutineAllowed = false;
+
+            // use List to replace this:
+            Vector2 p0 = routes[routeNumber].GetChild(0).position;
+            Vector2 p1 = routes[routeNumber].GetChild(1).position;
+            Vector2 p2 = routes[routeNumber].GetChild(2).position;
+            Vector2 p3 = routes[routeNumber].GetChild(3).position;
+
+            while (_tParam < 1)
+            {
+                _tParam += Time.deltaTime * _speedModifier;
+
+                _targetObjectPosition = Mathf.Pow(1 - _tParam, 3) * p0 +
+                                        3 * Mathf.Pow(1 - _tParam, 2) * _tParam * p1 +
+                                        3 * (1 - _tParam) * Mathf.Pow(_tParam, 2) * p2 +
+                                        Mathf.Pow(_tParam, 3) * p3;
+
+                transform.position = _targetObjectPosition;
+                
+                yield return new WaitForEndOfFrame();
+            }
+
+            _tParam = 0f;
+
+            _routeToGo += 1;
+
+            if (_routeToGo > routes.Length - 1)
+            {
+                _routeToGo = 0;
+            }
+
+            _coroutineAllowed = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/BezierCurve/Route.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace PocketLobby.Utility
+{
+    public class Route : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] _controlPoints;
+
+        private Vector2 _gizmosPosition;
+
+        private void OnDrawGizmos()
+        {
+            for (float t = 0; t <= 1; t += 0.02f)
+            {
+                _gizmosPosition = Mathf.Pow(1 - t, 3) * _controlPoints[0].position +
+                                 3 * Mathf.Pow(1 - t, 2) * t * _controlPoints[1].position +
+                                 3 * (1 - t) * Mathf.Pow(t, 2) * _controlPoints[2].position +
+                                 Mathf.Pow(t, 3) * _controlPoints[3].position;
+
+                Gizmos.DrawSphere(_gizmosPosition, 5f);
+            }
+            
+            Gizmos.DrawLine(_controlPoints[0].position, _controlPoints[1].position);
+            Gizmos.DrawLine(_controlPoints[2].position, _controlPoints[3].position);
+        }
+    }
+}
