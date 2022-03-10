9287c60cb0482eb8ec48717b4432b1b24a501280
Wes <weskao@yile.com.tw>
2022-03-10 18:39:21 +0800

    Use _coroutineAllowed to control

--- Assets/Scripts/Utility/BezierCurve/BezierFollow.cs
+++ Assets/Scripts/Utility/BezierCurve/BezierFollow.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace PocketLobby.Utility
@@ -7,48 +8,49 @@ namespace PocketLobby.Utility
     public class BezierFollow : MonoBehaviour
     {
         [SerializeField]
-        private Transform[] routes = null;
+        private Transform[] _routes = null;
+        
+        [SerializeField]
+        private bool _coroutineAllowed;
 
-        private int _routeToGo;
+        private List<Vector2> _nodePositionList = new List<Vector2>();
+
+        private int _routeIndexToGo;
         private float _tParam;
         private Vector2 _targetObjectPosition;
         private float _speedModifier;
-        private bool _coroutineAllowed;
+        private const int ROUTES_CHILD_COUNT = 4;
 
         private void Start()
         {
-            _routeToGo = 0;
+            _routeIndexToGo = 0;
             _tParam = 0f;
             _speedModifier = 0.5f;
-            _coroutineAllowed = true;
+            // _coroutineAllowed = true;
         }
 
         private void Update()
         {
-            if (_coroutineAllowed)
+            if (_coroutineAllowed && IsExistAnyRoute())
             {
-                StartCoroutine(GoByTheRoute(_routeToGo));
+                _nodePositionList.Clear();
+                StartCoroutine(GoByTheRoute(_routeIndexToGo));
             }
         }
-
+        
         private IEnumerator GoByTheRoute(int routeNumber)
         {
             _coroutineAllowed = false;
-
-            // use List to replace this:
-            Vector2 p0 = routes[routeNumber].GetChild(0).position;
-            Vector2 p1 = routes[routeNumber].GetChild(1).position;
-            Vector2 p2 = routes[routeNumber].GetChild(2).position;
-            Vector2 p3 = routes[routeNumber].GetChild(3).position;
+            SetNodePositionList(routeNumber);
 
             while (_tParam < 1)
             {
                 _tParam += Time.deltaTime * _speedModifier;
 
-                _targetObjectPosition = Mathf.Pow(1 - _tParam, 3) * p0 +
-                                        3 * Mathf.Pow(1 - _tParam, 2) * _tParam * p1 +
-                                        3 * (1 - _tParam) * Mathf.Pow(_tParam, 2) * p2 +
-                                        Mathf.Pow(_tParam, 3) * p3;
+                _targetObjectPosition = Mathf.Pow(1 - _tParam, 3) * _nodePositionList[0] +
+                                        3 * Mathf.Pow(1 - _tParam, 2) * _tParam * _nodePositionList[1] +
+                                        3 * (1 - _tParam) * Mathf.Pow(_tParam, 2) * _nodePositionList[2] +
+                                        Mathf.Pow(_tParam, 3) * _nodePositionList[3];
 
                 transform.position = _targetObjectPosition;
                 
@@ -57,14 +59,27 @@ namespace PocketLobby.Utility
 
             _tParam = 0f;
 
-            _routeToGo += 1;
+            _routeIndexToGo += 1;
 
-            if (_routeToGo > routes.Length - 1)
+            if (_routeIndexToGo > _routes.Length - 1)
             {
-                _routeToGo = 0;
+                _routeIndexToGo = 0;
             }
 
             _coroutineAllowed = true;
+        }
+
+        private void SetNodePositionList(int routeNumber)
+        {
+            for (var i = 0; i < ROUTES_CHILD_COUNT; i++)
+            {
+                _nodePositionList.Add(_routes[routeNumber].GetChild(i).position);
+            }
+        }
+        
+        private bool IsExistAnyRoute()
+        {
+            return _routes.Length > 0;
         }
     }
 }
